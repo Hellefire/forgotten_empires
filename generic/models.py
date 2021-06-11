6a6853f9bfeeb3b4165453da5911784de32834f1
Hellefire <aaronblairak@yahoo.co.uk>
2021-06-11 16:32:32 -0300

    Add generic app and models, move empires enums to generic, rename class app to charclass

---
+++ generic/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+class DeliveryMethod(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'deliverymethod'
+
+    def __str__(self):
+        return self.name
+
+
+class DurationType(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'durationtype'
+
+    def __str__(self):
+        return self.name
+
+
+class Effect(models.Model):
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'effect'
+
+    def __str__(self):
+        return self.description
+
+
+class TargetType(models.Model):
+    name = models.CharField(unique=True, max_length=128)
+
+    class Meta:
+        db_table = 'targettype'
+
+    def __str__(self):
+        return self.name
+
+
+class Version(models.Model):
+    version = models.CharField(unique=True, max_length=32)
+    created = models.DateTimeField()
+    notes = models.TextField()
+
+    class Meta:
+        db_table = 'version'
+
+    def __str__(self):
+        return self.version
