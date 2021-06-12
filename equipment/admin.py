b4ba247693815204ee7634c2bb80529dd3961757
Hellefire <aaronblairak@yahoo.co.uk>
2021-06-11 21:34:05 -0300

    Add equipment app models

--- equipment/admin.py
+++ equipment/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    ArmourType, ArmourType_Effect, Equipment, Weapon
+)
+
+
+admin.site.register(ArmourType)
+admin.site.register(ArmourType_Effect)
+admin.site.register(Equipment)
+admin.site.register(Weapon)
--- equipment/models.py
+++ equipment/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+from empires.stdfields.fields import EnumCharField
+
+from generic.models import Effect
+
+from .enums import equipmenttype, weapontype
+
+
+class ArmourType(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    points = models.IntegerField()
+    slots = models.IntegerField()
+    worn = models.IntegerField()
+
+    class Meta:
+        db_table = 'armourtype'
+
+    def __str__(self):
+        return self.name
+
+
+class ArmourType_Effect(models.Model):
+    armourtype = models.ForeignKey(ArmourType, models.RESTRICT)
+    effect = models.ForeignKey(Effect, models.RESTRICT)
+
+    class Meta:
+        db_table = 'armourtype_effect'
+        unique_together = (('armourtype', 'effect'),)
+
+    def __str__(self):
+        return '{} - {}'.format(str(self.armourtype), str(self.effect))
+
+
+class Equipment(models.Model):
+    name = models.CharField(unique=True, max_length=32)
+    equipmenttype = EnumCharField(
+        choices=equipmenttype.all(), max_length=equipmenttype.max_length())
+
+    class Meta:
+        db_table = 'equipment'
+
+    def __str__(self):
+        return self.name
+
+
+class Weapon(models.Model):
+    name = models.CharField(unique=True, max_length=64)
+    weapontype = EnumCharField(
+        choices=weapontype.all(), max_length=weapontype.max_length())
+
+    class Meta:
+        db_table = 'weapon'
+
+    def __str__(self):
+        return self.name
