bf66495de020eee9a92ac2b98e3d1e10b7097a25
Hellefire <aaronblairak@yahoo.co.uk>
2021-08-26 23:01:20 -0300

    Add SkillCost to admin

--- skill/admin.py
+++ skill/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 from .models import (
-    RacialSkillType, Skill, Race_RacialSkillType, Skill_RacialSkillType
+    RacialSkillType, Skill, SkillCost, Race_RacialSkillType, Skill_RacialSkillType
 )
 
 
 admin.site.register(RacialSkillType)
 admin.site.register(Skill)
+admin.site.register(SkillCost)
 admin.site.register(Race_RacialSkillType)
 admin.site.register(Skill_RacialSkillType)
